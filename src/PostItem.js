7b2eb142d67951467204cb53ad4f10e9c064a1a5
zhangcong <807448518@qq.com>
2018-11-13 18:39:22 +0800

    add new

--- src/PostItem.js
+++ src/PostItem.js
@@ -1,24 +1,71 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './PostItem.css'
+import like from './images/timg.jpg'
 
+//复杂逻辑的类式PostItem组件
 class PostItem extends Component {
-  constructor () {
-    super()
-    this.state({
-      vote: 0
+  constructor (props) {
+    super(props)
+    this.state = {
+      editing: false,  // 帖子是否处于编辑态
+      post: props.post 
+    }
+
+    this.handleVote = this.handleVote.bind(this)
+  }
+
+  // 这里应为构造函数、 方法、 访问器或属性
+  handleClick () {
+    let vote = this.state.vote
+    vote++
+    this.setState({
+      vote: vote
     })
   }
-  // 这里应为构造函数、 方法、 访问器或属性
   render () {
     const {title, author, date} = this.props
+    let vote = this.state.vote
     return (
       <li> {/*这个是作为ul标签内的组件，所以最外层应使用li标签*/}
         <div>{title}</div>
         <div>创建人：<span>{author}</span></div>
         <div>创建时间: <span>{date}</span></div>
-        <div><button>点赞</button></div>
+        <div><button onClick={this.handleClick}>点赞</button>&nbsp;<span>{vote}</span></div>
       </li>
     )
   }
 }
+
+// 简单功能的函数式PostItem组件
+// function  PostItem(props) {
+//   const handleClick = () => {
+//     props.onVote(props.post.id)
+//   }
+
+//   PostItem.propTypes = {
+//     post: PropTypes.shape({
+//       title: PropTypes.string,
+//       id: PropTypes.number,
+//       vote: PropTypes.number,
+//       date: PropTypes.string
+//     }).isRequired,
+//     onVote: PropTypes.func.isRequired
+//   }
+
+//   const {post} = props //只接收props中的post对象 等价于 const post = props.post
+//   // const post = props.post
+
+//   return (
+//     <li className="item">
+//       <div className="title">{post.title}</div>
+//       <div>创建人: <span>{post.author}</span></div>
+//       <div>创建时间: <span>{post.date}</span></div>
+//       <div className="like">
+//         <span><img src={like} onClick={handleClick} alt=""/></span><span>{post.vote}</span>
+//         </div>
+//     </li>
+//   )
+// }
 
 export default PostItem
--- src/PostList.js
+++ src/PostList.js
@@ -1,20 +1,111 @@
 import React, { Component } from 'react';
 import PostItem from './PostItem';
+import './PostList.css'
+
+// class PostList extends Component {
+//   render () {
+//     const data = [
+//       {title: '大家一起来讨论React吧', author: '张三', date: '2017-09-01 10:00', id:0},
+//       {title: '前端框架，你最爱哪个', author: '李四', date: '2017-09-01 10:00', id:1},
+//       {title: 'Web App的时代已经到来', author: '王五', date: '2017-09-01 10:00', id:2}
+//     ]
+
+//     return (
+//       <div>
+//         帖子列表
+//         <ul>
+//           {
+//             data.map(item => <PostItem key={item.id} title={item.title} author={item.author} date={item.date}/>)
+//           }
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+// 改进后的PostList组件
 
 class PostList extends Component {
-  render () {
-    const data = [
-      {title: '大家一起来讨论React吧', author: '张三', date: '2017-09-01 10:00', id:0},
-      {title: '前端框架，你最爱哪个', author: '李四', date: '2017-09-01 10:00', id:1},
-      {title: 'Web App的时代已经到来', author: '王五', date: '2017-09-01 10:00', id:2}
-    ]
+  constructor (props) {
+    super(props)
+    this.state = {
+      posts: [],
+      name: '',
+      password: ''
+    }
+    this.timeId = null // 定时器ID
+    this.handleVote = this.handleVote.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
+  componentDidMount () {
+    this.timeId = setTimeout(
+      () => {
+        this.setState({
+          posts: [
+            {title: '大家一起来讨论React吧', author: '张三', date: '2017-09-01 10:00', id:1, vote:0},
+            {title: '前端框架，你最爱哪个', author: '李四', date: '2017-09-01 10:00', id:2, vote:0},
+            {title: 'Web App的时代已经到来', author: '王五', date: '2017-09-01 10:00', id:3, vote:0}
+          ]
+        })
+      }, 1000)
+  }
+
+  componentWillUnmount () {
+    if (this.timeId) {
+      clearTimeout(this.timeId)
+    }
+  }
+
+  handleVote (id) {
+    // 根据帖子id进行过滤，找到待修改vote属性的帖子，返回新的posts对象
+    const posts = this.state.posts.map(item => {
+      const newItem = item.id === id? {...item, vote: ++item.vote} : item
+      return newItem
+    })
+
+    this.setState({
+      posts: posts
+      // posts
+    })
+  }
+
+  handleChange (event) {
+    const target = event.target
+    // console.log(target.name)
+    // console.log(target.value)
+    this.setState({
+      // name: target.value,
+      // password: target.value
+      [target.name]:target.value
+    })
+  }
+
+  handleSubmit (event) {
+    console.log('login successfully')
+    event.preventDefault()
+  }
+
+  render () {
     return (
       <div>
+        <div>
+          <form>
+            <label>
+              用户名:
+              <input type="text" name="name" value={this.state.name}  onChange={this.handleChange}/>
+            </label>
+            <label>
+              密码:
+              <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+            </label>
+          </form>
+        </div>
         帖子列表
         <ul>
           {
-            data.map(item => <PostItem key={item.id} title={item.title} author={item.author} date={item.date}/>)
+            this.state.posts.map(item => <PostItem key={item.id} post={item} onVote={this.handleVote} />)
           }
         </ul>
       </div>
